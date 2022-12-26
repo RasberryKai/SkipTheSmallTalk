59473fa6562774b8497df25fd60fafec47000a85
kai.kueppers <kai.kueppers@gi.com>
2022-12-26 13:02:42 +0100

    added onClick property on Header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,14 +1,28 @@
 import { Divider } from "@mantine/core"
 import { IconArrowBack } from "@tabler/icons"
 import { useNavigate } from "react-router"
+import React from "react"
 
-export default function Header(props: any) {
+interface HeaderProps {
+    children: React.ReactNode
+    onClick?: () => void
+    className?: string
+}
+
+export default function Header(props: HeaderProps) {
     const navigate = useNavigate()
 
     return (
         <div className={props.className}>
             <div className={"flex flex-row items-center"}>
-                <IconArrowBack color={"white"} className={"mr-2"} onClick={() => navigate(-1)} />
+                <IconArrowBack
+                    color={"white"}
+                    className={"mr-2"}
+                    onClick={() => {
+                        if (props.onClick) props.onClick()
+                        else navigate(-1)
+                    }}
+                />
                 <p className={"text-white text-4xl mb-2"}>{props.children}</p>
             </div>
             <Divider color={"white"} />
