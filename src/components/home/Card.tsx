052ab0a502fb9fb6e9aa46054a43089d3505d2ed
kai.kueppers <kai.kueppers@gi.com>
2023-01-01 13:18:04 +0100

    hotfix: Cannot read properties of undefined

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -18,7 +18,11 @@ export default function Card(props: CardProps) {
                 <div className={"ml-2 flex-col justify-start items-start w-full"} onClick={props.onClick}>
                     <p className={"text-black text-2xl flex items-start justify-start mb-2"}>{props.name}</p>
                     {props.owner && <p className={"text-black text-lg flex items-start justify-start"}>Owner: {props.owner}</p>}
-                    <p className={"text-black text-lg flex items-start justify-start"}>Players: {props.playerNames.join(", ")}</p>
+                    {props.playerNames.length > 0 && (
+                        <p className={"text-black text-lg flex items-start justify-start"}>
+                            Shared with: {props.playerNames.join(", ")}
+                        </p>
+                    )}
                 </div>
                 {/* Options */}
                 <Options id={props.id} isOwner={props.isOwner} />
