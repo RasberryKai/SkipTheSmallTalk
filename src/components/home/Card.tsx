a1d69553ed8233d5aff908a9a52f5387c6f10b6b
kai.kueppers <kai.kueppers@gi.com>
2023-01-01 13:25:46 +0100

    hotfix

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -18,7 +18,7 @@ export default function Card(props: CardProps) {
                 <div className={"ml-2 flex-col justify-start items-start w-full"} onClick={props.onClick}>
                     <p className={"text-black text-2xl flex items-start justify-start mb-2"}>{props.name}</p>
                     {props.owner && <p className={"text-black text-lg flex items-start justify-start"}>Owner: {props.owner}</p>}
-                    {props.playerNames.length > 0 && (
+                    {props.playerNames && props.playerNames.length > 0 && (
                         <p className={"text-black text-lg flex items-start justify-start"}>
                             Shared with: {props.playerNames.join(", ")}
                         </p>
