f4bd714be8a4b8fc60f0972e769735e820a34e9f
kai.kueppers <kai.kueppers@gi.com>
2023-01-04 19:22:40 +0100

    now updating finished attribute on levelGames once the last card is reached

--- src/components/play/ControlBar.tsx
+++ src/components/play/ControlBar.tsx
@@ -4,11 +4,14 @@ import ActionBar from "./ActionBar"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../../types"
 import RoundIconButton from "./RoundIconButton"
+import { supabase } from "../../lib/Supabase"
+import { dbTables } from "../../constants/keys"
 
 export default function ControlBar() {
     const dispatch = useDispatch()
     const cardsDone = useSelector((state: RootState) => state.game.cardsDone)
     const cardsLeft = useSelector((state: RootState) => state.game.cardsLeft)
+    const levelGameId = useSelector((state: RootState) => state.game.levelGameId)
 
     return (
         <div className={"flex flex-row w-full justify-between items-center w-[90%]"}>
@@ -22,6 +25,12 @@ export default function ControlBar() {
             <ActionBar />
             <RoundIconButton
                 onClick={() => {
+                    if (cardsLeft.length === 1)
+                        supabase
+                            .from(dbTables.levelGames)
+                            .update({ finished: true })
+                            .eq("id", levelGameId)
+                            .then(() => {})
                     if (cardsLeft.length > 0) dispatch(nextCard())
                 }}
             >
