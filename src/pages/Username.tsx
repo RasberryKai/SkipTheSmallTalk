1b0669b787994bfe0e7c425e7d5041fce5e2aaf8
kai.kueppers <kai.kueppers@gi.com>
2023-01-06 19:34:08 +0100

    Fix: duplicate profile entry

--- src/pages/Username.tsx
+++ src/pages/Username.tsx
@@ -58,7 +58,7 @@ export default function Username() {
             <AuthContainer
                 onSubmit={(e: any) => {
                     e.preventDefault()
-                    validateUsername()
+                    validateUsername().then()
                 }}
             >
                 <AuthHeader>Username</AuthHeader>
