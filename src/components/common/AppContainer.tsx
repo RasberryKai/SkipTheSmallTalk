96e21881ab0a5c57e92d63bab8bfb0a622b9a37a
kai.kueppers <kai.kueppers@gi.com>
2023-01-07 09:27:17 +0100

    improved scrolling behavior

--- src/components/common/AppContainer.tsx
+++ src/components/common/AppContainer.tsx
@@ -1,8 +1,13 @@
 export default function AppContainer(props: any) {
     document.body.classList.add("bg-primary-darker")
+    document.body.style.overflow = "hidden"
     const width = window.innerWidth
     return (
-        <div className={`w-full max-w-[500px] ${width >= 500 ? "rounded-2xl" : ""} h-full min-h-screen bg-primary-dark p-4`}>
+        <div
+            className={`w-full max-w-[500px] ${
+                width >= 500 ? "rounded-2xl" : ""
+            } h-screen min-h-screen bg-primary-dark p-4 overflow-scroll`}
+        >
             {props.children}
         </div>
     )
