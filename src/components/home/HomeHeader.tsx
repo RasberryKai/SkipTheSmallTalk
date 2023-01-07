c5c53c6401e21b3677cfc07581a183ab37fb7cf4
kai.kueppers <kai.kueppers@gi.com>
2023-01-07 09:40:36 +0100

    changed coin icon

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -8,14 +8,14 @@ export default function HomeHeader() {
             <img
                 src={require("../../assets/elementIcons/IceCream.png")}
                 alt={"Profile picture"}
-                className={"w-14 rounded-tl-[65%] rounded-2xl"}
+                className={"w-14 rounded-tl-[65%] rounded-2xl hover:cursor-pointer"}
             />
             <div className={"w-full flex flex-row items-center justify-center"}>
-                <IconCoin size={30} stroke={1.2} color={"#D4AF37"} />
+                <IconCoin size={34} stroke={1.3} color={"#e7cc61"} />
                 <p className={"text-white text-lg font-bold ml-2 text-white"}>0</p>
             </div>
             <div className={"w-full flex justify-end items-center pr-1"}>
-                <IconAlignRight size={30} stroke={1.5} color={colors.primaryGray} />
+                <IconAlignRight size={30} stroke={1.5} color={colors.primaryGray} className={"hover:cursor-pointer"} />
             </div>
         </div>
     )
